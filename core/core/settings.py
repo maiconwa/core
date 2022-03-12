1e4c8ac5ee733f07f4ccc2fae939fd8afcd946da
Maicon William <maicon.w.a@gmail.com>
2022-03-12 20:07:18 -0300

    added only json comment on settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -134,6 +134,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+        # commenting the BrowsableAPIRenderer will allow only Json to be shown
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
